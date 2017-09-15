7d17ab1754c9909d05852b71137b0d57cd41cdc8
iovxw <iovxw@outlook.com>
2017-09-15 18:12:00 +0800

    Switch to `h2`

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,207 @@
+#![feature(proc_macro, conservative_impl_trait, generators)]
+
 extern crate bytes;
 extern crate rustls;
-extern crate futures;
+extern crate futures_await as futures;
 extern crate tokio_core;
 extern crate tokio_io;
-extern crate tokio_proto;
 extern crate tokio_rustls;
+extern crate http;
+extern crate h2;
 
-mod http2;
+use std::net::SocketAddr;
+use std::collections::VecDeque;
+use std::rc::Rc;
+use std::cell::RefCell;
+
+use futures::prelude::*;
+use tokio_core::net::{TcpListener, TcpStream};
+use tokio_core::reactor::{Core, Handle};
+use tokio_io::{AsyncRead, AsyncWrite};
+use tokio_io::io::shutdown;
+use bytes::{BytesMut, Bytes, IntoBuf};
+use h2::{server as h2s, client as h2c};
+
+mod pool;
+use pool::*;
+
+macro_rules! poll {
+    ($e:expr) => ({
+        loop {
+            match $e {
+                ::futures::__rt::Ok(::futures::Async::Ready(e)) => {
+                    break ::futures::__rt::Ok(e)
+                }
+                ::futures::__rt::Ok(::futures::Async::NotReady) => {}
+                ::futures::__rt::Err(e) => {
+                    break ::futures::__rt::Err(e)
+                }
+            }
+            yield
+        }
+    })
+}
+
+fn client(listen_addr: SocketAddr, server_addr: SocketAddr) {
+    let mut lp = Core::new().unwrap();
+
+    let listener = TcpListener::bind(&listen_addr, &lp.handle()).unwrap();
+    println!("Listening on: {}", listen_addr);
+    println!("Proxying to: {}", server_addr);
+
+    let pool = pool::H2ClientPool::new(lp.handle(), server_addr);
+    let done = listener.incoming().for_each(|(client, client_addr)| {
+        client_handle(client, pool.handle())
+            .map(move |(client_to_server, server_to_client)| {
+                println!(
+                    "{:?}: {}, {}",
+                    client_addr,
+                    client_to_server,
+                    server_to_client
+                );
+            })
+            .or_else(|e| {
+                println!("{:?}", e);
+                Ok(())
+            })
+    });
+    lp.run(done).unwrap();
+}
+
+#[async]
+fn client_handle(client: TcpStream, pool_handle: PoolHandle) -> Result<(usize, usize), h2::Error> {
+    let req = http::Request::builder()
+        .method(http::Method::CONNECT)
+        .uri("https://example.com")
+        .body(())
+        .unwrap();
+    let (client_reader, client_writer) = client.split();
+    let mut stream = await!(pool_handle.request(req, false))?;
+    let (parts, body) = poll!(stream.poll_response())?.into_parts();
+    if parts.status != http::StatusCode::OK {
+        unimplemented!();
+    }
+    let server_to_client = copy_from_h2(body, client_writer);
+    let client_to_server = copy_to_h2(client_reader, stream);
+    await!(client_to_server.join(server_to_client))
+}
+
+#[async]
+fn copy_from_h2<W: AsyncWrite + 'static, B: Stream<Item = Bytes, Error = h2::Error> + 'static>(
+    src: B,
+    mut dst: W,
+) -> Result<usize, h2::Error> {
+    let mut counter = 0;
+    #[async]
+    for buf in src {
+        let mut buf = buf.into_buf();
+        let n = poll!(dst.write_buf(&mut buf))?;
+        counter += n;
+    }
+    shutdown(dst);
+    Ok(counter)
+}
+
+trait SendData<B: IntoBuf> {
+    fn send_data(&mut self, data: B, end_of_stream: bool) -> Result<(), h2::Error>;
+}
+
+impl<B: IntoBuf> SendData<B> for h2c::Stream<B> {
+    fn send_data(&mut self, data: B, end_of_stream: bool) -> Result<(), h2::Error> {
+        h2c::Stream::send_data(self, data, end_of_stream)
+    }
+}
+impl<B: IntoBuf> SendData<B> for h2s::Stream<B> {
+    fn send_data(&mut self, data: B, end_of_stream: bool) -> Result<(), h2::Error> {
+        h2s::Stream::send_data(self, data, end_of_stream)
+    }
+}
+
+#[async]
+fn copy_to_h2<R: AsyncRead + 'static, H: SendData<Bytes> + 'static>(
+    mut src: R,
+    mut dst: H,
+) -> Result<usize, h2::Error> {
+    let mut counter = 0;
+    let mut buf = BytesMut::with_capacity(1024);
+    loop {
+        let n = poll!(src.read_buf(&mut buf))?;
+        if n == 0 {
+            dst.send_data(buf.take().freeze(), true)?;
+            break;
+        } else {
+            dst.send_data(buf.take().freeze(), false)?;
+        }
+        counter += n;
+    }
+    Ok(counter)
+}
+
+fn server(listen_addr: SocketAddr, server_addr: SocketAddr) {
+    let mut lp = Core::new().unwrap();
+    let handle = lp.handle();
+
+    let listener = TcpListener::bind(&listen_addr, &lp.handle()).unwrap();
+    println!("Listening on: {}", listen_addr);
+    println!("Proxying to: {}", server_addr);
+
+    let done = listener.incoming().for_each(move |(client, client_addr)| {
+        let handle2 = handle.clone();
+        let connection = h2s::Server::handshake(client)
+            .and_then(move |conn| {
+                let handle = handle2.clone();
+                conn.for_each(move |(request, stream)| {
+                    server_handle(handle.clone(), request, stream)
+                        .map(move |(client_to_server, server_to_client)| {
+                            println!(
+                                "{:?}: {}, {}",
+                                client_addr,
+                                client_to_server,
+                                server_to_client
+                            );
+                        })
+                        .or_else(|e: h2::Error| {
+                            println!("{:?}", e);
+                            Ok(())
+                        })
+                })
+            })
+            .map_err(|e| println!("{:?}", e));
+
+        handle.spawn(connection);
+        Ok(())
+    });
+    lp.run(done).unwrap();
+}
+
+#[async]
+fn server_handle(
+    handle: Handle,
+    request: http::Request<h2s::Body<Bytes>>,
+    stream: h2s::Stream<Bytes>,
+) -> Result<(usize, usize), h2::Error> {
+    let (parts, body) = request.into_parts();
+    if parts.method != http::Method::CONNECT {
+        unimplemented!();
+    }
+    let addr = "127.0.0.1:8080".parse().unwrap();
+    let server = await!(TcpStream::connect(&addr, &handle))?;
+    let (server_reader, server_writer) = server.split();
+    let server_to_client = copy_to_h2(server_reader, stream);
+    let client_to_server = copy_from_h2(body, server_writer);
+    await!(client_to_server.join(server_to_client))
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
+        let client_addr = "127.0.0.1:3345".parse().unwrap();
+        let server_addr = "127.0.0.1:3346".parse().unwrap();
+        let s = ::std::thread::spawn(move || server(server_addr, server_addr));
+        let c = ::std::thread::spawn(move || client(client_addr, server_addr));
+        s.join().unwrap();
+        c.join().unwrap();
     }
 }
---
+++ src/pool.rs
@@ -0,0 +1,115 @@
+use std::net::SocketAddr;
+use std::collections::VecDeque;
+use std::rc::Rc;
+use std::cell::RefCell;
+
+use futures::prelude::*;
+use tokio_core::net::TcpStream;
+use tokio_core::reactor::Handle;
+use bytes::Bytes;
+use h2::{self, client as h2c};
+use http::Request;
+
+#[derive(Clone)]
+pub struct PoolHandle {
+    addr: SocketAddr,
+    handle: Handle,
+    pool: Rc<RefCell<VecDeque<h2c::Client<TcpStream, Bytes>>>>,
+}
+
+#[derive(Clone)]
+pub struct H2ClientPool(PoolHandle);
+
+impl H2ClientPool {
+    pub fn new(handle: Handle, addr: SocketAddr) -> H2ClientPool {
+        let h = PoolHandle {
+            addr: addr,
+            handle: handle,
+            pool: Rc::new(RefCell::new(VecDeque::new())),
+        };
+        H2ClientPool(h)
+    }
+
+    pub fn handle(&self) -> PoolHandle {
+        self.0.clone()
+    }
+}
+
+impl Future for H2ClientPool {
+    type Item = ();
+    type Error = ();
+    fn poll(&mut self) -> Poll<(), ()> {
+        if Rc::strong_count(&self.0.pool) == 1 {
+            let wired_strems: usize = self.0
+                .pool
+                .borrow()
+                .iter()
+                .map(h2c::Client::num_wired_streams)
+                .sum();
+            if wired_strems == 0 {
+                // free memory
+                return Ok(Async::Ready(()));
+            }
+        }
+
+        for idx in 0..self.0.pool.borrow().len() {
+            let mut pool = self.0.pool.borrow_mut();
+            let remove = {
+                let client = pool.get_mut(idx).unwrap();
+                match client.poll() {
+                    Ok(Async::Ready(())) => true,
+                    Ok(Async::NotReady) => false,
+                    Err(e) => {
+                        eprintln!("{:?}", e);
+                        true
+                    }
+                }
+            };
+            if remove {
+                pool.remove(idx);
+            }
+        }
+        Ok(Async::NotReady)
+    }
+}
+
+impl PoolHandle {
+    pub fn request<'a>(
+        &self,
+        request: Request<()>,
+        end_of_stream: bool,
+    ) -> impl Future<Item = h2c::Stream<Bytes>, Error = h2::Error> + 'a {
+        let s = self.clone();
+        async_block! {
+            let mut client = await!(s.pop())?;
+            let stream = client.request(request, end_of_stream)?;
+            s.pool.borrow_mut().push_back(client);
+            Ok(stream)
+        }
+    }
+
+    fn new_client<'a>(
+        &self,
+    ) -> impl Future<Item = h2c::Client<TcpStream, Bytes>, Error = h2::Error> + 'a {
+        TcpStream::connect(&self.addr, &self.handle)
+            .map_err(Into::into)
+            .and_then(|socket| h2c::Client::handshake(socket))
+    }
+
+    fn pop<'a>(&self) -> impl Future<Item = h2c::Client<TcpStream, Bytes>, Error = h2::Error> + 'a {
+        let s = self.clone();
+        async_block!{
+            let client = s.pool.borrow_mut().pop_front();
+            let mut client = match client {
+                Some(x) => x,
+                None => await!(s.new_client())?,
+            };
+
+            if client.poll_ready().unwrap().is_not_ready() {
+                await!(s.pop())
+            } else {
+                Ok(client)
+            }
+        }
+    }
+}
